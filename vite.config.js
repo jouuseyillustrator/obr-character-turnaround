4a5291db698ea3b4e6c5c9fba0b71539b28753b5
jouuseyillustrator <jouuseyillustrator@gmail.com>
2026-09-02 09:47:38 -0300

    Set explicit subfolder base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     host: true,
-    base: './',
+    base: '/obr-character-turnaround/',
     allowedHosts: true,
     cors: true,
     headers: {
